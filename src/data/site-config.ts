6f40684894ecbc7cfd3723a0f06736e28b84c08d
raschomakuin <rodripalacio14@gmail.com>
2026-02-22 12:10:21 -0300

    Update website URL in site configuration

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'https://rodrigopalacio.github.io',
+    website: 'https://raschomakuin.github.io',
     avatar: {
         src: avatar,
         alt: 'Ethan Donovan'
